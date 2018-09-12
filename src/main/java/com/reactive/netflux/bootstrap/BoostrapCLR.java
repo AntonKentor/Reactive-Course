39c149e01f33f22552dc8769d42f91364dd0073e
amosenbacher <anton.mosenbacher@kentor.se>
2018-09-12 13:11:41 +0200

    Changed title and id values in boostrap class since they where showing wrong values.

--- src/main/java/com/reactive/netflux/bootstrap/BoostrapCLR.java
+++ src/main/java/com/reactive/netflux/bootstrap/BoostrapCLR.java
@@ -38,7 +38,7 @@ public class BoostrapCLR implements CommandLineRunner {
                 "Meet the Fluxes",
                 "Lord of the Fluxes")
 
-                .map(title -> new Movie(title, UUID.randomUUID().toString()))
+                .map(title -> new Movie(UUID.randomUUID().toString(), title ))
                 .flatMap(movieRepository::save))
                 .subscribe(null, null, () -> {
                     movieRepository.findAll().subscribe(System.out::println);
