b8a23bd277b26ed9d3e9d2921b9ce262ce856340
amosenbacher <anton.mosenbacher@kentor.se>
2018-09-12 12:44:11 +0200

    Added component to boostrap class. Some refactoring.

--- src/main/java/com/reactive/netflux/bootstrap/BoostrapCLR.java
+++ src/main/java/com/reactive/netflux/bootstrap/BoostrapCLR.java
@@ -4,10 +4,16 @@ import com.reactive.netflux.domain.Movie;
 import com.reactive.netflux.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
+/*
+ * CommandLineRunner is a Spring boot specific class
+ * thats starts when applications starts
+ * */
+@Component
 public class BoostrapCLR implements CommandLineRunner {
 
     private final MovieRepository movieRepository;
@@ -21,11 +27,19 @@ public class BoostrapCLR implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         //clear old data
-        movieRepository.deleteAll().block();
+        movieRepository.deleteAll()
+                .thenMany(
+                        Flux.just(
+                "Silence of the Lambdas",
+                "AEon Flux",
+                "Enter the Mono<Void>",
+                "The Fluxxinator",
+                "Back to the future",
+                "Meet the Fluxes",
+                "Lord of the Fluxes")
 
-        Flux.just("Silence of the Lambdas", "AEon Flux", "Enter the Mono<Void>", "The Fluxxinator", "Back to the future", "Meet the Fluxes", "Lord of the Fluxes")
                 .map(title -> new Movie(title, UUID.randomUUID().toString()))
-                .flatMap(movieRepository::save)
+                .flatMap(movieRepository::save))
                 .subscribe(null, null, () -> {
                     movieRepository.findAll().subscribe(System.out::println);
                 });
