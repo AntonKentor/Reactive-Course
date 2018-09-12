371acfc6cc4a4edb223c5a56b950421c4e3f99cf
amosenbacher <anton.mosenbacher@kentor.se>
2018-09-12 12:57:52 +0200

    Added Service implementation. Added AllArgsConstructor to MovieEvent.

--- src/main/java/com/reactive/netflux/services/MovieServiceImpl.java
+++ src/main/java/com/reactive/netflux/services/MovieServiceImpl.java
@@ -2,23 +2,37 @@ package com.reactive.netflux.services;
 
 import com.reactive.netflux.domain.Movie;
 import com.reactive.netflux.domain.MovieEvent;
+import com.reactive.netflux.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.util.Date;
+
 public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
 
     @Override
     public Flux<MovieEvent> events(String movieId) {
-        return null;
+        return Flux.<MovieEvent>generate(movieEventSynchronousSink -> {
+            movieEventSynchronousSink.next(new MovieEvent(movieId, new Date()));
+        }).delayElements(Duration.ofSeconds(1));
     }
 
     @Override
     public Mono<Movie> getMoviebyId(String id) {
-        return null;
+        return movieRepository.findById(id);
     }
 
     @Override
     public Flux<Movie> getAllMovies() {
-        return null;
+        return movieRepository.findAll();
     }
 }
